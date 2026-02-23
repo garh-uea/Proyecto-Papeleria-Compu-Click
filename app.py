82f088e8474feda55c849302ef00c91f60dfa1fa
Gustavo Rodríguez <ga.rodriguezh@uea.edu.ec>
2026-02-22 20:58:59 -0500

    Proyecto Papeleria Compu Click Semana 10

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
+# ================== INICIO ==================
 @app.route('/')
-def Hello_world():
-    return "Bienevenido a Papelería Compu Click en la ciudad de Tena"
+def inicio():
+    return render_template('index.html')
 
-@app.route('/usuario/<nombre>')
-def usuario(nombre):
-    return f'¡Hola {nombre}! Qué gusto tenerte en Papelería Compu Click!'
-
+# ================== CONTACTOS ==================
 @app.route('/contactos')
 def contactos():
-    return f'📩 Escríbenos a compuclick_tena@gmail.com o al WhatsApp 0999532532. ¡Estamos listos para ayudarte!'
+    return render_template('contactos.html')
+
+# ================== SOBRE NOSOTROS ==================
+@app.route('/sobre-nosotros')
+def about():
+    return render_template('sobre-nosotros.html')
+
+# ================== REGISTRO ==================
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
+
+# ================== USUARIO DINÁMICO ==================
+@app.route('/usuario/<nombre>')
+def usuario(nombre):
+    return render_template('index.html', nombre=nombre)
+
+# ================== CONTACTOS ==================
+@app.route('/contactos')
+def contactos_dinamico():
+    return render_template('contactos.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
